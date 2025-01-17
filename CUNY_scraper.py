c06a14477afffe33c966faac407961cdbf75c468
ShabirZ <shabirzahir1202@gmail.com>
2025-01-16 19:36:22 -0500

    got through first webpage

--- CUNY_scraper.py
+++ CUNY_scraper.py
@@ -4,12 +4,15 @@ import requests
 class CUNYScraper:
     def __init__(self, course, term):
         self.course = course
-        self.url = "https://globalsearch.cuny.edu/CFGlobalSearchTool/search.jsp"
+        self.url = "https://globalsearch.cuny.edu/CFGlobalSearchTool/CFSearchToolController"
         self.term = term
+        self.session = requests.Session()
         professors = self.get_professors()
+        
     def load_first_webpage(self):
         """
-        Initial Payload:
+        Request Method: POST
+        first webPage Payload:
             selectedInstName: Queens College | 
             inst_selection: QNS01
             selectedTermName: 2025 Spring Term
@@ -29,7 +32,17 @@ class CUNYScraper:
             term_value: 1249
             next_btn: Next
         """
-        pass
+
+        payload = {
+            "selectedInstName": "Queens College |", 
+            "inst_selection": "QNS01",
+            "selectedTermName": "2025 Spring Term",
+            "term_value": "1252",
+            "next_btn": "Next",
+        }
+        test = self.session.post(self.url, data=payload)
+        print(test.text)
+        return
     def get_professors(self):
-        
-        pass
+        self.load_first_webpage()
+CUNYScraper(1,2)
