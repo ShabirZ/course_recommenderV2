33e404aa75a37ad3138a443d439d12779fd2569f
ShabirZ <shabirzahir1202@gmail.com>
2025-01-22 17:58:12 -0500

    made function to get total # profs in QC RMP

--- RMP_scraper.py
+++ RMP_scraper.py
@@ -15,3 +15,21 @@ I will drop any name not in my Course Table and create some sort of Table/CSV th
 class rmpProfessorIdScraper:
     def __init__(self):
         self.QC_url = "https://www.ratemyprofessors.com/search/professors/231?q=*"
+        self.session = requests.session()
+    
+    def load_front_page(self):
+        def find_prof_count(soup):
+            #<h1 data-testid="pagination-header-main-results">4235<!-- --> professor<!-- -->s<!-- --> <span>at <b>CUNY Queens College</b></span></h1>
+            # replace: (<)    ---->  (>) then split to get strictly number
+            prof_count_html = soup.find('h1')
+            return int(str(prof_count_html).replace('<', '>').split('>')[2])
+
+        front_page = self.session.get(self.QC_url)
+        soup = BeautifulSoup(front_page.text, 'html.parser')
+
+        prof_count = find_prof_count(soup)
+
+
+
+a = rmpProfessorIdScraper()
+a.load_front_page()
