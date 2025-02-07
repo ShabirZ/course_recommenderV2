7a66f3c72fdf071465ea8b6455d39fba66fed3d9
ShabirZ <shabirzahir1202@gmail.com>
2025-02-07 17:16:45 -0500

    connected database with backend, added .env file to hide db info

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql2');
+require('dotenv').config(); // Load the .env file
 
+const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database,
+  });
+connection.connect();
 app.get('/', (req, res) => {
-    console.log('here');
-    res.send("test");
-});
+
+    connection.query("SELECT DISTINCT course_name, course_code FROM courseschedule;", (err, results) => {
+      if (err) {
+        console.error('Error:', err);
+        res.status(500).send('Database query error');
+      } else {
+        console.log(results);
+        res.json(results); // Sends results back as JSON
+      }
+    });
+  });
 
 app.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
+
 });
